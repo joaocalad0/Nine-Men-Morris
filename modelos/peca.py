3af14fc01c64712c429774384c1ef97e2182fbfa
Gusion. <127810981+joaocalad0@users.noreply.github.com>
2025-12-15 18:25:11 +0000

    comments updated

--- modelos/peca.py
+++ modelos/peca.py
@@ -1,22 +1,37 @@
+# Marco Francisco - 26963
+# Joao Calado - 24295
 
+# Função que cria uma "peca" se for valida.
+# Aceita apenas 'X', 'O' ou ' '
 def cria_peca(marca):
     if not eh_peca(marca):
         raise ValueError("cria_peca: argumento invalido")
-    return marca
+    return marca # Retorna a marca se valida
 
+# Cria uma copia da peca recebida.
+# Neste caso, como as peças são imutaveis, equivalente a retornar o mesmo valor.
 def cria_copia_peca(arg):
     return cria_peca(arg)
 
+#Verifica se o argumento e uma peca valida ('X', 'O' ou ' ').
 def eh_peca(arg):
     return arg in ["X", "O", ' ']
+
+# Compara duas pecas para ver se sao iguais.
+# Apenas retorna True se ambos forem peças validas e iguais.
 def pecas_iguais(p1, p2):
     return (
         eh_peca(p1) 
         and eh_peca(p2) 
         and p1 == p2
     )
+
+#converte a peca para string
 def peca_para_str(marca):
     return f'[{marca}]'
+
+
+#converte a peca para int
 def peca_para_inteiro(marca):
     if marca == 'X':
         return 1
@@ -24,6 +39,10 @@ def peca_para_inteiro(marca):
         return -1
     else:
         return 0
+
+
+#Converte um int de volta para peca:
+# 1 -> 'X', -1 -> 'O', 0 -> ' '
 def inteiro_para_peca(valor):
     if valor == 1:
         return cria_peca('X')
--- modelos/posicao.py
+++ modelos/posicao.py
@@ -1,27 +1,42 @@
-# João Calado - 24295
+# Joao Calado - 24295
+# Marco Francisco - 26963
 from raiz.utils import alpha_para_index, index_para_alpha
 from raiz.constantes import *
 
+
+# Cria posicao no tabuleiro a partir de coluna e linha
+# Garante que posicao valida antes de criar
 def cria_posicao(coluna, linha):
     if (not eh_posicao([coluna, linha])):
         raise ValueError("cria_posicao: argumentos invalidos")
     return [coluna.lower(), linha]
 
+
+#cria uma cópia de uma posicao
+#converte dict para list
 def cria_copia_posicao(pos):
     if(type(pos) == dict): pos = list(pos.values())
     return cria_posicao(pos[0], pos[1])
 
+
+#retorna a coluna de uma posicao
 def obter_pos_c(pos):
     return pos[0]
 
+#retorna a linha de uma posicao como string
 def obter_pos_l(pos):
     return str(pos[1])
 
+
+#garante que o argumento da posicao e valida
 def eh_posicao(arg):
     return ( isinstance(arg, list) and arg[0].isalpha() and str(arg[1]).isdigit() 
     and alpha_para_index(arg[0]) > 0 and alpha_para_index(arg[0]) <= TAMANHO_TABULEIRO 
     and int(arg[1]) > 0 and int(arg[1]) <= TAMANHO_TABULEIRO )
 
+
+#Compara duas posicoes para ver se sao iguais
+#apenas retorna True se ambas forem posicoes validas e tiverem mesma coluna e linha
 def posicoes_iguais(p1, p2):
     return (
         eh_posicao(p1) 
@@ -30,8 +45,12 @@ def posicoes_iguais(p1, p2):
         and p1[1] == p2[1]
     )
 
+
+#converte uma posicao para string no formato "A1", "B2",
 def posicao_para_str(pos):
     return f'{obter_pos_c(pos)}{obter_pos_l(pos)}'
+
+
 
 def str_para_posicao(s):
     if (len(s) != 2):
@@ -41,6 +60,8 @@ def str_para_posicao(s):
     if (not eh_posicao([coluna, linha])):
         raise ValueError("str_para_posicao: argumento invalido")
     return cria_posicao(coluna, linha)
+
+
 
 def obter_posicoes_adjacentes(pos):
     # retorna um tuplo com as posicoes adjacentes
